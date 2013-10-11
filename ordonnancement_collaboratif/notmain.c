fcb4081acc1125124aabebecea70249cbfd946c9
Dugue Josselin <jdugue@if501-219-14.insa-lyon.fr>
2013-10-11 10:25:28 +0200

    switch to v2

--- ordonnancement_collaboratif/notmain.c
+++ ordonnancement_collaboratif/notmain.c
@@ -55,35 +55,4 @@ notmain ( void )
 
   switch_to(&ctx_A);
 }
-struct ctx_s ctx_init;
 
-void
-ping()
-{
-  while (1) {
-    switch_to(&ctx_B);
-    switch_to(&ctx_B);
-  }
-}
-
-void
-pong()
-{
-  while (1) {
-    switch_to(&ctx_A);
-    switch_to(&ctx_A);
-    switch_to(&ctx_A);
-  }
-}
-
-//------------------------------------------------------------------------
-int
-notmain ( void )
-{
-  init_ctx(&ctx_A, ping, STACK_SIZE);
-  init_ctx(&ctx_B, pong, STACK_SIZE);
-
-  current_ctx = &ctx_init;
-
-  switch_to(&ctx_A);
-}
