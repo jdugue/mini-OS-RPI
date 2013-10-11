55a60a26f7d41a364c2fdcb4e51b6db6dd1ba267
Dugue Josselin <jdugue@if501-219-14.insa-lyon.fr>
2013-10-11 09:22:54 +0200

    switch bug plus

--- ordonnancement_collaboratif/dispatcher.c
+++ ordonnancement_collaboratif/dispatcher.c
@@ -3,18 +3,12 @@
 
 void switch_to(struct ctx_s* ctx){
 	
-	uint32_t* varLR = current_ctx->pc;
-	uint32_t* varSP = current_ctx->sp;
-	__asm("mov %0, lr" : "=r"(varLR)  );
-	__asm("mov %0, sp" : "=r"(varSP));
+	__asm("mov %0, lr" : "=r"(current_ctx->pc));
+	__asm("mov %0, sp" : "=r"(current_ctx->sp));
 
-
-	current_ctx->pc = ctx->pc;
-	current_ctx->sp = ctx->sp;
+	current_ctx = ctx;
 
 	//Restauration
-	varLR = current_ctx->pc;
-	varSP = current_ctx->sp;
-	__asm("mov lr, %0" : : "r"(varLR));
-	__asm("mov sp, %0" : : "r"(varSP));	
+	__asm("mov lr, %0" : : "r"(current_ctx->pc));
+	__asm("mov sp, %0" : : "r"(current_ctx->sp));	
 }
